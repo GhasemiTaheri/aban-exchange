3a0125c08169d6fa962dcb0482b29ca33d4fe290
Mojtaba <mghasemi13786@gmail.com>
2024-12-17 18:20:29 +0000

    add order table

--- aban_exchange/exchange/models.py
+++ aban_exchange/exchange/models.py
@@ -1,6 +1,36 @@
 from django.conf import settings
-from django.db import models
+from django.core.validators import MinValueValidator
+from django.db.models import CASCADE
+from django.db.models import ForeignKey
+from django.db.models import PositiveIntegerField
+
+from aban_exchange.utils.db.models import BaseModel
 
 
-class Order(models.Model):
+class BaseOrder(BaseModel):
+    user = ForeignKey(
+        to=settings.AUTH_USER_MODEL,
+        on_delete=CASCADE,
+        verbose_name="Order owner",
+        related_name="%(class)s_related",
+        related_query_name="%(class)ss",
+    )
+    price = PositiveIntegerField(verbose_name="Currency Price")
+    amount = PositiveIntegerField(
+        validators=[MinValueValidator(1)],
+        verbose_name="Amount (USD)",
+    )
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return f"user: {self.user_id}, {self.amount} on {self.price}"
+
+
+class Order(BaseOrder):
+    pass
+
+
+class ArchiveOrder(BaseOrder):
     pass
